7fd1285eebdc47cee3218e47e9ed85109dfd719f
Zamp98 <33207987+Zamp98@users.noreply.github.com>
2020-10-13 10:57:56 -0300

    c1
    c1

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/globals/jquery/index.d.ts"/>
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+$("#btn1").click(function () {
+    alert("botao kkkk")
+    console.log("o botaõ foi clicas kkkk")
+})
